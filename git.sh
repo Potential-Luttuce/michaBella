9d1db47d243bd619a5624f3d8ee641ed5d95a058
Michael Cuesta <cuestamichael0@gmail.com>
2023-07-09 16:14:59 -0400

    changed timing in git.sh - structual impovements to test file

--- git.sh
+++ git.sh
@@ -4,20 +4,12 @@
 
 clear && git status
 
-# sleep 2
-
 echo -e '\n'
 
 neofetch
 
 echo "Leaving bash..."
 
-sleep 1
-
 python3 git.py
 
-sleep 1
-
 echo -e 'Entering Bash ^=^=^=^= Hello from Bash!\n'
-
-# sleep 2

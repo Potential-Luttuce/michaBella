0d420c865411c81cb019a3a1348a9ce27eed06d3
Michael Cuesta <cuestamichael0@gmail.com>
2023-07-09 13:55:28 -0400

    edit git.py

--- git.py
+++ git.py
@@ -7,17 +7,17 @@ import os
 print('Entering Python_-_-_-_-Hello From Python!')
 
 print('\t\tPYTHON-START')
-save = input('\nDo any files changes need to be ADDED?(y,n): ')
+save = input('\nDo any files changes need to be ADDED? [y,n]: ')
 if (save == 'y'):
     os.system('git add . && clear && git status')
     backup = ''
     while backup != 'n':
-        backup = input('\nNeed to save again?(y,n): ')
+        backup = input('\nNeed to save again? [y,n]: ')
         if (backup == 'y'):
             os.system('git add . && clear && git status')
-    commit = input('\nDo you want to COMMIT & PUSH these saved changes?(y,n): ')
+    commit = input('\nDo you want to COMMIT & PUSH these saved changes? [y,n]: ')
     if (commit == 'y'):
-        message = input('\nPlease enter COMMIT MESSAGE(free text): ')
+        message = input('\nPlease enter COMMIT MESSAGE [Free text]: ')
         os.system(f"git commit -m '{message}' && git push")
         os.system("clear && neofetch") 
         print('\nChanges have been saved, commited, and pushed to Github Repo.')

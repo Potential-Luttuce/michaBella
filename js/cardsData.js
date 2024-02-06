489d2de9a60d596a3e1c735519a56931366145e2
Mike Cuesta <cuestamichael0@gmail.com>
2024-02-06 18:02:50 -0500

    Edit to homepage sidebar text, created js file 'cardsData' for holding unique card data

---
+++ js/cardsData.js
@@ -0,0 +1,11 @@
+//JS FILE FOR CARDS DATA
+
+const cardsData = [
+    {id: 'card1', name: 'Card One', img: '', text: 'Test Text'}
+    ,{id: 'card2', name: 'Card Two', img: '', text: 'Test Text'}
+    ,{id: 'card3', name: 'Card Three', img: '', text: 'Test Text'}
+    ,{id: 'card4', name: 'Card Four', img: '', text: 'Test Text'}
+    ,{id: 'card5', name: 'Card Five', img: '', text: 'Test Text'}
+];
+
+export default cardsData;

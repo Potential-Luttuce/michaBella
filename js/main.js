cdef2f24a8d6651767fd7d7e28ee4ccb1612c532
Mike <Cuestamichael0@gmail.com>
2024-02-13 18:55:59 -0500

    Added specific text to cards 2020

--- js/main.js
+++ js/main.js
@@ -60,31 +60,43 @@ function createAllCards(dataMatrix) {
 const cardData2020 = [
     // 2020
     { id: 'card1', title: 'Some Light Thrifting', img: '../../images/2020/mike-at-garment.jpg',
-    desc: "Ah yes, the garment district!! :)"},
-    { id: 'card2', title: 'You\'re Favorite View!', img: '../../images/2020/memDrive.png',
-    desc: "Ah yes, mem drive!! :)" },
+    desc: `Ah yes, the garment district!
+    We had such a fun day in Boston together. 
+    I attempted to recreate a very cute insta post you made a few days earlier lol
+        How'd I do?`},
+    { id: 'card2', title: 'Your Favorite View!', img: '../../images/2020/memDrive.png',
+    desc: `You loved this view of the city so much, for good reason.
+        But damn woman you almost broke my neck trying to park for this pic!!` },
     { id: 'card3', title: 'Ojitos Lindos', img: '../../images/2020/ojitos-lindos.jpg',
-    desc: "Ah yes, tour first pic :)" },
+    desc: `Need I say more? You looked so beautiful this day and this has always
+        been one of my favorite pictures of you.` },
     { id: 'card4', title: 'The Sweet Beginning', img: '../../images/2020/our-first-pic.jpg',
-    desc: "Ah yes, the garment district!! :)" },
+    desc: `Our official first picture together! Look how little we both are. 
+        thinking back I have never had as much fun with someone as I have with you, babe.` },
     { id: 'card5', title: 'A Bad Day To Be Shoes', img: '../../images/2020/broken-shoes.jpg',
-    desc: "Ah yes, the garment district!! :)" },
+    desc: `Okay. Laught it up. I quite literally blew a shoe out trying to make 
+        you laugh.` },
     { id: 'card6', title: 'A Light Selfie', img: '../../images/2020/light-selfie.jpg',
-    desc: "Ah yes, the garment district!! :)" },
+    desc: `I was so nervous this night and this selfie really made me nervous. 
+        But it was an incredbly fun date!` },
     { id: 'card7', title: 'You...Give Me Wings', img: '../../images/2020/mikes-wings.jpg',
-    desc: "Ah yes, the garment district!! :)" },
+    desc: `this one si going to be cheesy, but you make me so happy and have always been
+        their for me when I needed you. You are my Angel :,)` },
     { id: 'card8', title: 'Too Close For Comfort', img: '../../images/2020/couch-selfie.jpg',
-    desc: "Ah yes, the garment district!! :)" },
+    desc: `So what if we say miles apart on your love seat? 'Heres mom and dad back
+        in the day'` },
     { id: 'card9', title: 'Naughty AND Nice', img: '../../images/2020/goodvsevil.jpg',
-    desc: "Ah yes, the garment district!! :)" },
+    desc: `We are so funny lol but this picture is another top 10 moment` },
     { id: 'card10', title: 'Some-ting-sweet', img: '../../images/2020/something-sweet.jpg',
-    desc: "Ah yes, the garment district!! :)" },
+    desc: `Okay so this one might be random but damn how good do these look??` },
     { id: 'card11', title: 'Newbury Adventures', img: '../../images/2020/cartier.jpg',
-    desc: "Ah yes, the garment district!! :)" },
+    desc: `You are my diamond, you are so bright in my life and there isn't
+        another person that could compare to you` },
     { id: 'card12', title: 'A Christmas Miracle!', img: '../../images/2020/christmas-card.jpg',
-    desc: "Ah yes, the garment district!! :)" },
+    desc: `Our unofficial christmad card from back in the day` },
     { id: 'card13', title: 'Grogu Green', img: '../../images/2020/grogu-green.jpg',
-    desc: "Ah yes, the garment district!! :)" }
+    desc: `You still got this make up? The ring camera light on point, helmet on point,
+        makeup on point, you look stunning` }
 ];
 const cardData2021 = [
     //2021
